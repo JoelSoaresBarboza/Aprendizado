937ef15472622b987bdd71674b4640f1b6e7ce3c
Joel Soares <113053264+JoelSoaresBarboza@users.noreply.github.com>
2022-09-13 15:09:49 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,54 +1,27 @@
-function verificar() {
-var data = new Date ()
-var ano = data.getFullYear()
-var fano = document.getElementById('txtano')
-var res = document.getElementById ('res')
-
-if(fano.value.length == 0 || fano.value > ano){
-    window.alert ('[ERRO] Verifique os dados e tente novamente!')
+function contar() {
+    var ini = document.getElementById('inicio1')
+    var fim = document.getElementById ('fim2')
+    var pas = document.getElementById ('passo3')
+    var res = document.getElementById ('res')
+if (ini.value.length==0 || fim.value.length==0 || pas.value.length==0) {
+    window.alert ('Preencha os dados e tente novamente!')
+    res.innerHTML = 'Impossível Contar'
 }
-
-else{
-var fsex = document.getElementsByName ('radsex')
-var idade = ano - Number(fano.value)
-var genero = ''
-var img = document.createElement('img')
-img.setAttribute('id','foto')
-if (fsex[0].checked) {
-    genero = 'Homem'
-    if (idade>=0 && idade<=2){
-        img.setAttribute = ('src','foto-bebem.png')
-    } else if (idade<12){
-        img.setAttribute = ('src','foto-menino.png')
-    }
-    else if (idade<22){
-        img.setAttribute = ('src','foto-adolescentem.png')
-    }
-    else if (idade<60){
-        img.setAttribute = ('src','foto-homem.png')
-    }
-    else {img.setAttribute = ('src','foto-idoso.png')}
-   
-
-} else {
-    genero = 'Mulher'
-    if (idade>=0 && idade<=2){
-        img.setAttribute = ('src','foto-bebef.png')
-    } else if (idade<12){
-        img.setAttribute('src','foto-menina.png')
+    else{
+        res.innerHTML = 'Contando: </br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+        if (p<=0) { p=1}
+        if (i<f) {
+        for (var c = i ; c<= f; c+=p) {  // quando for criar condição considerar um efeito imaginário
+            res.innerHTML += `${c} \u{1F449}` //ficou faltando essa observação no innerhtml '''+=''' e não precisava de parentese
+        }} else {
+            for (var c = i ; c>=f; c-=p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-    else if (idade<=22){
-        img.setAttribute = ('src','foto-adolescentef.png')
+        res.innerHTML +=`\u{1F3C1}` //res.innerhtml é ===== depois as aspas
     }
-    else if (idade<=60){
-        img.setAttribute = ('src','foto-mulher.png')
-    }
-    else {img.setAttribute = ('src','foto-idosa.png')}
-}
-res.innerHTML = `Você é ${genero} e tem ${idade} anos!`
-res.appendChild(img)
-}
-
-
-
+   
 }
