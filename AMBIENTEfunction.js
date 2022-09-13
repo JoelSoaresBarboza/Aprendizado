f2c95681035e31658632707ff50ccff6bf2f6aa6
Joel Soares <113053264+JoelSoaresBarboza@users.noreply.github.com>
2022-09-13 18:24:55 -0300

    Add files via upload

---
+++ AMBIENTEfunction.js
@@ -0,0 +1,23 @@
+/*function parimp (n){
+if (n%2 == 0) {
+    return 'Par!'
+} else {
+    return 'Impar!'
+}
+
+}
+var res = parimp (223)
+console.log (res)*/ // esse código funciona e é um exemplo de como funciona as funções no js
+
+/*function soma(n1=0,n2=0) {
+
+return n1 +n2
+}
+
+console.log (soma (2))*/
+
+var v = function (x){
+    return x*2
+}
+console.log (v(4))
+
