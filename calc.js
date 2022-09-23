1af139d1f7c185b8b2e9bd1ac15f6fe199db742c
Joel Soares <113053264+JoelSoaresBarboza@users.noreply.github.com>
2022-09-23 16:24:45 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,7 @@
+var somafunc = require ("./soma")
+
+var multiii = require ("./multi")
+
+console.log (somafunc (1,2))
+
+console.log (multiii(3,5))//como usar esse arquivo para puxar arquivo de outras funcoes na mesma pasta
